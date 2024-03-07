c01d158bb4955b1106c31d9baae8866888ebba28
Chumi379 <1509227127@qq.com>
2024-03-08 00:28:50 +0800

    1

---
+++ src/main/java/top/chumi/alkaid/AlkaidApplication.java
@@ -0,0 +1,13 @@
+package top.chumi.alkaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkaidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlkaidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/chumi/alkaid/AlkaidApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chumi.alkaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkaidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
